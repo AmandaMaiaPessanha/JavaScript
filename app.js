8bb7110b34711eb0a4343ea6c2c3805004a75895
Amanda Maia <ampessanha@stone.com.br>
2017-08-18 22:06:27 -0300

    Add button + config JS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var main =  function () {
   "use strict";
 
   $(".commet-input button").on("click", function(event) {
-    console.log ("Hello World!");
+    $(".comments").append("<p>this is a new comment</p>");
   });
 };
 
