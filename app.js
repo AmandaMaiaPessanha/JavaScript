ee0333777430453a29e54a995fc5cfc74017fc3f
Amanda Maia <ampessanha@stone.com.br>
2017-08-21 14:35:20 -0300

    Add scroll and JS reusable

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var main =  function(){
   "use strict";
-  $(".commet-input button").on("click", function (event){
+  //Aqui estou criando uma função no qual vai ser reutilizada nas linhas de comando 14 e 17.
+  var addCommentFromInputBox = function () {
     var $new_comment;
     if ($(".commet-input input").val() !== "") {
       $new_comment = $("<p>").text($(".commet-input input").val());
@@ -9,17 +10,15 @@ var main =  function(){
       $new_comment.fadeIn();
       $(".commet-input input").val("");
     }
+  };
+  $(".commet-input button").on("click", function (event){
+    //Na linha 16 e na linha 20 eu to puxando a função no qual eu define lá em cima.
+    addCommentFromInputBox();
   });
-
   $(".commet-input input").on("keypress", function (event){
-    var $new_comment;
-      if (event.keyCode === 13) {
-        if ($(".commet-input input").val() !== "") {
-          var $new_comment = $("<p>").text($(".commet-input input").val());
-          $(".comments").append($new_comment);
-          $(".commet-input input").val("");
-        }
-      }
+    if (event.keyCode === 13) {
+      addCommentFromInputBox();
+    }
   });
 };
 $(document).ready(main);
