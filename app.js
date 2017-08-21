8d9d7d5983f50d33f5a77e0d4bb943ca2fa16806
Amanda Maia <ampessanha@stone.com.br>
2017-08-21 13:51:37 -0300

    Add style div all

--- app.js
+++ app.js
@@ -6,5 +6,16 @@ var main =  function(){
     $new_comment.text(comment_text);
     $(".comments").append($new_comment);
   });
+
+  $(".commet-input input").on("keypress", function (event){
+    var $new_comment;
+      if (event.keyCode === 13) {
+        if ($(".commet-input input").val() !== "") {
+          var $new_comment = $("<p>").text($(".commet-input input").val());
+          $(".comments").append($new_comment);
+          $(".commet-input input").val("");
+        }
+      }
+  });
 };
 $(document).ready(main);
